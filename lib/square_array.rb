94bdb0bf11c0325dbc58a5bd8d4fdb83d1bee6b7
Johnson Liu <jyliu17@gmail.com>
2020-08-25 01:09:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  num = []
   count = 0
   while count < numbers.length do
-    new_array << numbers[count] ** 2
+    numbers[count] ** 2
     count += 1
   end
    puts new_array
